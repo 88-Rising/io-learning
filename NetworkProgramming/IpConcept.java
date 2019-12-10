76b12e1489f99a90cbde0bfc748a11c10d465c97
88-Rising <348514635@qq.com>
2019-12-10 23:01:34 +0800

    InetAddress对象测试

--- NetworkProgramming/IpConcept.java
+++ NetworkProgramming/IpConcept.java
@@ -1,10 +1,13 @@
 package NetworkProgramming;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class IpConcept {
 
     /*
     *
-    * Ip地址：用来表示网络中的一个通信实体的地址 通信实体可以是计算机，路由器等 比如
+    * Ip地址（用来定位计算机）：用来表示网络中的一个通信实体的地址 通信实体可以是计算机，路由器等 比如
     *       互联网的每一个服务器，都要有自己的IP地址，而每一个局域网的计算机要通信也要配置IP地址。
     *       路由器是连接两个或多个网络的网络设备
     *
@@ -14,4 +17,9 @@ public class IpConcept {
     * 特殊的IP：本机地址 私有地址（属于费非注册地址，专门位组织机构内部使用）
     *
     * */
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println(addr.getHostAddress());//返回 本机地址
+        System.out.println(addr.getHostName());//输出计算机命
+    }
 }
